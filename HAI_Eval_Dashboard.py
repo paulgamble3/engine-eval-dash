aae93eb22acaf2b0700f1206a3cf98540fa2afd4
Paul Gamble <paulgamble@Hippocraticai-Paul-Gamble.local>
2024-01-22 17:13:53 -0800

    first

---
+++ HAI_Eval_Dashboard.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import json
+import pandas as pd
+
+# wide
+st.set_page_config(layout="wide")
+
+st.title("HAI Eval Dashboard")
+
+# for each engine
+# in a folder
+# load the various subsections
+
+# the results shown in each table
+# will come from a single file
+# the order in the file will determine the order in the table
+# the columns of the table will be the keys into the objects
+
+# write a loader+displayer that generalizes across all engine results
+
+def load_display(results_fn, results_dict):
+    with open(results_fn) as f:
+        results = json.load(f)
+
+    df_rows = []
+
+    for result in results:
+        row = [result["date"], result["run_name"], result["benchmark_version"]]
+        for key in results_dict:
+            row.append(result["results"][key])
+        df_rows.append(row)
+
+    df = pd.DataFrame(df_rows, columns=["Date", "Run Name", "Benchmark"] + list(results_dict.values()))
+
+    st.write(df)
+
+
+
+
+## Kickout Detector 
+kickout_detector_results = './results/kickout_detector/ko_detector_results.json'
+kickout_detector_results_dict = {
+    "accuracy": "Overall Accuracy",
+    "TPR": "TPR",
+    "TNR": "TNR",     
+}    
+st.subheader("Kickout Detector")
+st.write("n = 1250")
+load_display(kickout_detector_results, kickout_detector_results_dict)
+
+## Kickout Evaluator
+kickout_evaluator_results = './results/kickout_evaluator/kickout_evaluator_results.json'
+kickout_evaluator_results_dict = {
+    "accuracy": "Overall Accuracy",
+    "true_positive_rate": "TPR",
+    "true_negative_rate": "TNR",
+    "false_positive_rate": "FPR", 
+    "false_negative_rate": "FNR",
+}    
+st.subheader("Kickout Evaluator")
+st.write("n = 565")
+load_display(kickout_evaluator_results, kickout_evaluator_results_dict)
+
+
+
+## Kickout Detector - end to end
+kod_ete_results = './results/ko_detector_end_to_end/KOD_ete.json'
+kod_ete_results_dict = {
+    "Accuracy - 1 KO": "Accuracy - 1 KO",
+    "Accuracy - 2 KO": "Accuracy - 2 KO",
+    "Accuracy - 3 KO": "Accuracy - 3 KO"  
+}    
+st.subheader("Kickout Detector - End to End")
+load_display(kod_ete_results, kod_ete_results_dict)
+
+## Kickout Evaluator - end to end 
+koe_ete_results = './results/ko_evaluator_end_to_end/KOE_ete.json'
+koe_ete_results_dict = {
+    "Accuracy - 1 KO": "Accuracy - 1 KO",
+    "Accuracy - 2 KO": "Accuracy - 2 KO",
+    "Accuracy - 3 KO": "Accuracy - 3 KO"     
+}    
+st.subheader("Kickout Evaluator - End to End")
+load_display(koe_ete_results, koe_ete_results_dict)
+
+## Lab Engine
+lab_engine_results = './results/lab_engine/lab_results.json'
+lab_engine_results_dict = {
+    "overall_acc": "Overall Accuracy",
+    "label_acc": "Label Accuracy",
+    "value_acc": "Value Accuracy",     
+}    
+st.subheader("Lab Engine")
+st.write("n = 359")
+load_display(lab_engine_results, lab_engine_results_dict)
